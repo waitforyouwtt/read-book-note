41065a3d479a43b651f0fe0a878accb5c937e45d
luoxian <wtt19940512577>
2020-11-10 10:08:54 +0800

    读书笔记

---
+++ src/main/java/com/yidiandian/ReadBookNoteApplication.java
@@ -0,0 +1,13 @@
+package com.yidiandian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadBookNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadBookNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yidiandian/ReadBookNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yidiandian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadBookNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
